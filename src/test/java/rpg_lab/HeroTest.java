d6751fabad81de2f00462dbc8515af272010d657
krasimiranova <krasy_nova@yahoo.com>
2023-11-24 13:43:36 +0100

    Hero test - anonymous dependecy + mockito

--- src/test/java/rpg_lab/HeroTest.java
+++ src/test/java/rpg_lab/HeroTest.java
@@ -2,6 +2,7 @@ package rpg_lab;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
@@ -17,32 +18,36 @@ public class HeroTest {
 
         // creating an anonymous instance of the dependency needed
         // it returns void so we can implement it without writing anything
-        Weapon fakeWeapon = new Weapon() {
-            @Override
-            public void attack(Target target) {
+        // Weapon fakeWeapon = new Weapon() {
+            // @Override
+            // public void attack(Target target) {}
+        // };
 
-            }
-        };
+        Weapon fakeWeapon = Mockito.mock(Weapon.class);
 
-        Target fakeTarget = new Target() {
-            @Override
-            public boolean isDead() {
-                return true;
-            }
+        // Target fakeTarget = new Target() {
+           // @Override
+            // public boolean isDead() {
+                // return true;
+            // }
 
-            @Override
-            public int giveExperience() {
-                return EXPERIENCE;
-            }
+            // @Override
+            // public int giveExperience() {
+                // return EXPERIENCE;
+            // }
 
-            @Override
-            public void takeAttack(int attackPoints) {
+            // @Override
+            // public void takeAttack(int attackPoints) {
 
-            }
-        };
+            // }
+        // };
+
+        Target targetFake = Mockito.mock(Target.class);
+        Mockito.when(targetFake.isDead()).thenReturn(true);
+        Mockito.when(targetFake.giveExperience()).thenReturn(EXPERIENCE);
 
         this.hero = new Hero(NAME, fakeWeapon);
-        this.hero.attack(fakeTarget);
+        this.hero.attack(targetFake);
         assertEquals(EXPERIENCE, this.hero.getExperience());
     }
 
